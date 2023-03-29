33dbe5627b06d978f9361d02fb30a14af7bd8b34
Chantal Goethals <chantalgoethals@gmail.com>
2023-03-29 13:03:29 -0600

    Feat: add water droplet

---
+++ src/_waterDroplet.js
@@ -0,0 +1,16 @@
+import "./styles/waterDroplets.css"
+import React from 'react'
+
+const _waterDroplet = () => {
+
+    
+
+  return (
+    <div>
+        <section className="water-droplet">   
+        </section>
+    </div>
+  )
+}
+
+export default _waterDroplet
